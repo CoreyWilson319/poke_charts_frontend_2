dd472afc8fdfad6058f4d9de67c35beb0d88d483
Corey Wilson <72322079+CoreyWilson319@users.noreply.github.com>
2021-04-27 12:17:54 -0400

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer bg-dark">
+      <div className="container text-center">
+        <span className="text-muted">
+          @2020 Built by Corey Wilson (during COVID)
+        </span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
